1fa2de792c7db0295c71bb2363e5e6464a56b4c3
unknown <justinrathbone01@gmail.com>
2021-01-15 10:56:37 -0600

    more progress on user info being added into database

--- myrefapplication/src/backend/user.service.ts
+++ myrefapplication/src/backend/user.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth'
 import { Router } from '@angular/router';
 import { IUser } from "src/backend/user"
+import { AngularFireDatabase } from '@angular/fire/database';
 
 
 @Injectable({
@@ -11,8 +12,12 @@ export class UserService {
 
   isLoggedIn = false;
   currentUser: IUser = null;
+  database: AngularFireDatabase;
 
-  constructor(public firebaseAuth: AngularFireAuth, private router: Router) { }
+  constructor(public firebaseAuth: AngularFireAuth, private router: Router, private db: AngularFireDatabase) {
+    this.database = db;
+    
+   }
 
   async signin(email: string, password: string)
   {
@@ -20,6 +25,8 @@ export class UserService {
     .then(res=>{
         this.isLoggedIn = true;
         localStorage.setItem('user',JSON.stringify(res.user));
+        //this.currentUser = this.database.list('/users', ref => ref.orderByChild('UserUID').equalTo(res.user.uid))[0]
+        this.getUserInfo("sup");
         this.router.navigate(['/landing'])
     }).catch(_error =>{
         console.log(_error);
@@ -33,6 +40,7 @@ export class UserService {
     .then(res=>{
       this.isLoggedIn = true;
       localStorage.setItem('user',JSON.stringify(res.user));
+      
       this.router.navigate(['/landing'])
     }).catch(_error =>{
       console.log(_error);
@@ -41,11 +49,18 @@ export class UserService {
   }
 
   logout()
-  {
-    
+  { 
     this.firebaseAuth.signOut();
     localStorage.removeItem('user');
     this.router.navigate(['/login'])
+  }
+
+  async getUserInfo(UserUID: string){
+    await this.db.list('/users').valueChanges().subscribe({
+      next: users => {
+        users.filter(user => console.log(user));
+      }
+    })
   }
 
 }
