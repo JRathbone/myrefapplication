8ea069dbc6008dfb5c52345b5ddc91625aa428a0
JRathbone <justinrathbone01@gmail.com>
2021-01-23 11:15:12 -0600

    added todo list and dialog to assigned games component

--- myrefapplication/src/app/Components/landing/assigned-games/assigned-games.component.ts
+++ myrefapplication/src/app/Components/landing/assigned-games/assigned-games.component.ts
@@ -1,6 +1,9 @@
+import { Inject } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { IGame } from 'src/backend/game';
 import { scheduleService } from 'src/backend/schedule.service';
+import { UserService } from 'src/backend/user.service';
 
 
 @Component({
@@ -12,13 +15,15 @@ export class AssignedGamesComponent implements OnInit {
 
   
   assignedGames: IGame[] = [];
+  selectedGame: IGame;
 
-  constructor(private scheduleService: scheduleService) { 
+  constructor(private scheduleService: scheduleService,public dialog: MatDialog, private userHandler: UserService) { 
   
     scheduleService.getSchedule().subscribe({
       next: games => {
-        console.log(games);
-        this.assignedGames = games.filter(game => game.hasBeenApprovedOrDeclined == false);
+        
+        this.assignedGames = games.filter(game => game.hasBeenApprovedOrDeclined == false
+          && (game.centerReferee == userHandler.currentUser.displayName || game.assistantReferee1 == userHandler.currentUser.displayName || game.assistantReferee2 == userHandler.currentUser.displayName));
       }
     })
   }
@@ -26,4 +31,26 @@ export class AssignedGamesComponent implements OnInit {
   ngOnInit(): void {
     
   }
+
+  openDialog(gamenumber: Number): void
+  {
+    this.selectedGame = this.assignedGames.filter(games => games.gameNumber == gamenumber)[0]
+    const dialogRef = this.dialog.open(DialogContentExampleDialog, {
+      data: this.selectedGame
+   });
+    
+  }
+}
+
+@Component({
+  selector: 'assigned-games-dialog',
+  templateUrl: 'assigned-games-dialog.html',
+})
+export class DialogContentExampleDialog {
+  private selectedGame: IGame;
+  constructor(public dialogRef: MatDialogRef<DialogContentExampleDialog>,@Inject(MAT_DIALOG_DATA) public data: IGame) {
+    
+  }
+
+
 }
