a976bf9db7b3b69db5a73b80e8ba47367a3974f6
JRathbone <justinrathbone01@gmail.com>
2021-03-09 19:44:19 -0600

    new game form

--- myrefapplication/src/app/app.module.ts
+++ myrefapplication/src/app/app.module.ts
@@ -2,40 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from "@angular/material/icon";
+import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { LandingComponent } from '../app/Components/landing/landing.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AssignedGamesComponent } from './Components/landing/assigned-games/assigned-games.component';
-import { MatButtonModule} from '@angular/material/button';
-import { UpcominggamesComponent, UpcomingGamesDialog } from './Components/landing/upcoming-games/upcominggames.component';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  UpcominggamesComponent,
+  UpcomingGamesDialog,
+} from './Components/landing/upcoming-games/upcominggames.component';
 import { RecentgamesComponent } from './Components/landing/recentgames/recentgames.component';
 import { RouterModule } from '@angular/router';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
-import { AngularFireDatabaseModule } from '@angular/fire/database'
-import {MatDialogModule} from '@angular/material/dialog';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { MatDialogModule } from '@angular/material/dialog';
 import { LoginComponent } from './Components/login/login.component';
-import { DashboardComponent } from './Components/landing/dashboard/dashboard.component'
+import { DashboardComponent } from './Components/landing/dashboard/dashboard.component';
 import { MatCardModule } from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SignupComponent } from './Components/signup/signup.component';
-import {AssignedGamesDialog} from './Components/landing/assigned-games/assigned-games.component';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { AssignedGamesDialog } from './Components/landing/assigned-games/assigned-games.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { ScheduleComponent } from './Components/schedule/schedule.component';
 import { MatTableModule } from '@angular/material/table';
-import { CreateNewGameComponent } from './Components/schedule/create-new-game/create-new-game.component'  
+import { CreateNewGameComponent } from './Components/schedule/create-new-game/create-new-game.component';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     AssignedGamesComponent,
-    UpcominggamesComponent, 
+    UpcominggamesComponent,
     RecentgamesComponent,
     LoginComponent,
     DashboardComponent,
@@ -43,25 +47,24 @@ import { CreateNewGameComponent } from './Components/schedule/create-new-game/cr
     AssignedGamesDialog,
     UpcomingGamesDialog,
     ScheduleComponent,
-    CreateNewGameComponent
+    CreateNewGameComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule, 
+    MatToolbarModule,
     BrowserAnimationsModule,
     MatIconModule,
     MatGridListModule,
     HttpClientModule,
     MatButtonModule,
-    RouterModule.forRoot([ 
-      {path: '', component: LoginComponent},
-      {path: 'login', component: LoginComponent},
-      {path: 'landing', component: LandingComponent},
-      {path: 'signup', component: SignupComponent},
-      {path: 'schedule', component: ScheduleComponent},
-      {path: 'create-new-game', component: CreateNewGameComponent}
-      
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'landing', component: LandingComponent },
+      { path: 'signup', component: SignupComponent },
+      { path: 'schedule', component: ScheduleComponent },
+      { path: 'create-new-game', component: CreateNewGameComponent },
     ]),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
@@ -72,10 +75,10 @@ import { CreateNewGameComponent } from './Components/schedule/create-new-game/cr
     FormsModule,
     ReactiveFormsModule,
     MatProgressBarModule,
-    MatTableModule
-    
+    MatTableModule,
+    MatSelectModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
